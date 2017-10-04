21af7820ff2249ec617b15d64ab96da344efb575
DesC <cahill.desmond@gmail.com>
2017-10-04 09:22:18 +0000

    gradient change and fixed scaling

---
+++ asset/js/script.js
@@ -0,0 +1,6 @@
+// $(document).ready(function() {
+//     $("#myCollapsingList").on("hover", function(){
+//         $("ul li a").css("background-color", "white");
+//         $("this").css("background-color", "red");
+//     })
+// })
